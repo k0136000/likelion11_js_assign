1bc10c1dc60fa7445aacd14205ea9a288e98130d
김민규 <>
2022-12-26 16:05:22 +0900

    feat : todo list 기본틀 작성

---
+++ js/date.js
@@ -0,0 +1,15 @@
+const nowDate = document.getElementById("date");
+const nowDayOfWeek = document.getElementById("dayOfWeek");
+const WEEKDAY = ['일','월','화','수','목','금','토'];
+function getDates() {
+  const date = new Date();
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString();
+  const day = date.getDate().toString();
+  const dayOfWeek = WEEKDAY[date.getDay()]
+  nowDate.innerText = `${year}년 ${month}월 ${day}일`;
+  nowDayOfWeek.innerText = `${dayOfWeek}요일`;
+}
+
+getDates();
+setInterval(getDates, 1000);
---
+++ js/todoList.js
@@ -0,0 +1,24 @@
+let input = document.getElementById('input'); 
+let addBtn = document.getElementById('addBtn');
+let lists = document.getElementById('list');
+
+addBtn.addEventListener('click', function(){
+    
+    var list = document.createElement('li');    
+    if (!input.value)            
+        alert('내용을 입력해 주세요!');
+    else
+    {
+        list.innerText = input.value;  
+        lists.appendChild(list);       
+        input.value= "";               
+    }
+
+    list.addEventListener('click', function(){     
+        list.style.color = "#a0a0a0"
+        list.style.textDecoration = "line-through";
+    })
+    list.addEventListener('dblclick', function(){  
+        lists.removeChild(list);
+    })
+})
